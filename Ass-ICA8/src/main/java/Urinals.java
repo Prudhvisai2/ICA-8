47a81ef9baeaae6dcfc94f4e2ca9711404ff6810
Prudhvisai2 <112651253+Prudhvisai2@users.noreply.github.com>
2022-10-27 22:08:31 -0700

    Add files via upload

---
+++ Ass-ICA8/src/main/java/Urinals.java
@@ -0,0 +1,18 @@
+public class Urinals {
+    void openFile(){
+
+    }
+    void getString(){
+
+    }
+    void countUrinals(){
+
+    }
+    public boolean correctString(){
+        return true;
+    }
+    public static void main(String[] args){
+        System.out.println("");
+    }
+
+}
