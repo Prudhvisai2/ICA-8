346874c09c13ef51ce745d1a06564ed11e4e51ec
Prudhvisai2 <pgummire@asu.edu>
2022-10-28 21:30:26 -0700

    added testwriterfile

--- Ass-ICA8/src/main/java/UrinalsTest.java
+++ Ass-ICA8/src/main/java/UrinalsTest.java
@@ -5,5 +5,11 @@ import java.util.ArrayList;
 public class UrinalsTest {
 
 
-   
+    @Test
+    void testWriteFile(){
+        ArrayList<String> s = new ArrayList<String>();
+        s.add("hello");
+        Urinals u = new Urinals();
+        u.writeFile(s);
+    }
 }
